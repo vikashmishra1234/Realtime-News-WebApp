a5d7f4182c4825193396e24e66ca29f582130132
vikash mishra <vikashmishra8371@gmail.com>
2024-02-22 17:38:37 +0530

    last commit

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,68 +1,67 @@
-import React, {  useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
 import Spinner from "./Spinner";
 const NewsCard = (props) => {
   const [page, setPage] = useState(1);
   const [totalArt, setTotalArt] = useState("");
-  const [Loading,setLoading]=useState(false);
-
-  
-    const getData = async () => {
-      setLoading(true)
-      let data = await fetch(
-        `https://newsapi.org/v2/top-headlines?category=${props.category}&country=in&apiKey=48e55c194ddd4511afa3f2227239fc27&page=1&pageSize=15`
-      );
-      let Info = await data.json();
-      setLoading(false)
-      setTotalArt(Info.totalResults);
-
-      setNews(Info.articles);
-     
-    };
-    useEffect(()=>{
-
-      getData();
-    },[])
+  const [Loading, setLoading] = useState(false);
   const [News, setNews] = useState();
 
-    console.log(News,"avtu")
+  useEffect(() => {
+    const getData = async () => {
+      setLoading(true);
+    try {
+        let data = await fetch(
+          `https://newsapi.org/v2/top-headlines?category=${props.category}&country=in&apiKey=48e55c194ddd4511afa3f2227239fc27&page=1&pageSize=15`
+        );
+        let Info = await data.json();
+      
+       
+        setTotalArt(Info.totalResults);
+        console.log(Info.articles);
+        setNews(Info.articles)
+        console.log("this iz ", News);
+      }
+     catch (error) {console.log(error.message)} finally{setLoading(false)}
+    }
+
+
+    getData();
+    
    
+  }, []);
 
-
-
-
-
-  
   const handlePrevious = async () => {
     setPage(page - 1);
-    setLoading(true)
+    setLoading(true);
     let data = await fetch(
-      `https://newsapi.org/v2/top-headlines?category=${props.category}&country=in&apiKey=48e55c194ddd4511afa3f2227239fc27&page=${
+      `https://newsapi.org/v2/top-headlines?category=${
+        props.category
+      }&country=in&apiKey=48e55c194ddd4511afa3f2227239fc27&page=${
         page - 1
       }&pageSize=15`
     );
     let Info = await data.json();
-    setLoading(false)
+    setLoading(false);
     setTotalArt(Info.totalResults);
 
     setNews(Info.articles);
-
   };
 
   const handleNext = async () => {
-  
     if (page + 1 > Math.ceil(totalArt / 15)) {
-      
     } else {
       setPage(page + 1);
-      setLoading(true)
+      setLoading(true);
       let data = await fetch(
-        `https://newsapi.org/v2/top-headlines?category=${props.category}&country=in&apiKey=48e55c194ddd4511afa3f2227239fc27&page=${
+        `https://newsapi.org/v2/top-headlines?category=${
+          props.category
+        }&country=in&apiKey=48e55c194ddd4511afa3f2227239fc27&page=${
           page + 1
         }&pageSize=15`
       );
       let Info = await data.json();
-      setLoading(false)
+      setLoading(false);
       setTotalArt(Info.totalResults);
       console.log(Info.totalResults, "this is total result");
       setNews(Info.articles);
@@ -72,28 +71,28 @@ const NewsCard = (props) => {
   return (
     <div className="container w-100">
       <h2 className="pb-4 pt-3 fs-1 m-auto mb-3 text-center"> Top-Headlines</h2>
-     {Loading?<Spinner/>:''}
+      {Loading ? <Spinner /> : ""}
       <div className="row">
-        {News?News.map((news) => (
-          
-          <div className="col-md-4 mb-3"  key={news.url}>
-            <Card
-              title={news.title ? news.title : ""}
-              imgUrl={
-                news.urlToImage
-                  ? news.urlToImage
-                  : "https://images.moneycontrol.com/static-mcnews/2024/02/market_up_higher-3-770x433.jpg"
-              }
-             
-              content={
-                news.content
-                  ? news.content
-                  : "Read More about it on clicking the button below"
-              }
-              more={news.url}
-            />
-          </div>
-        )):''}
+        {News
+          ? News.map((news) => (
+              <div className="col-md-4 mb-3" key={news.url}>
+                <Card
+                  title={news.title ? news.title : ""}
+                  imgUrl={
+                    news.urlToImage
+                      ? news.urlToImage
+                      : "https://images.moneycontrol.com/static-mcnews/2024/02/market_up_higher-3-770x433.jpg"
+                  }
+                  content={
+                    news.content
+                      ? news.content
+                      : "Read More about it on clicking the button below"
+                  }
+                  more={news.url}
+                />
+              </div>
+            ))
+          : ""}
       </div>
       <div className="d-flex w-100 justify-content-around mt-3">
         <button
@@ -108,7 +107,7 @@ const NewsCard = (props) => {
           type="button"
           className="btn btn-dark"
           onClick={handleNext}
-          disabled = {page + 1 > Math.ceil(totalArt / 15)}
+          disabled={page + 1 > Math.ceil(totalArt / 15)}
         >
           Next
         </button>
